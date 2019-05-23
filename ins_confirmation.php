75a1ef737554c46175d99b16bfade0d48a3d226e
Azarote <azarotee@gmail.com>
2019-05-23 11:33:51 +0200

    Inscription
    -Modification majeur

--- ins_confirmation.php
+++ ins_confirmation.php
@@ -2,28 +2,65 @@
 <head>
 
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="icon" href="images/favicon.ico">
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel="stylesheet" href="css/boutique.css">
+		<link rel="stylesheet" href="css/camera.css">
+		<link rel="stylesheet" href="css/form.css">
+		<link rel="stylesheet" type="text/css" href="css/boutiqueBox.css">
 
 	<title>T-VORE Confirmation Inscription</title>
 </head>
 
 <body>
 
-<div class="site">
-
-	<header></header>
+		<header class="page1">
+			<div class="container_12">
+				<div class="grid_12">
+					<h1><a href="#"><img src="images/logo.png"></a></h1>
+					<div class="menu_block">
+						<nav>
+							<ul class="sf-menu">
+								<li class="current men"><a  href="index.php">Accueil</a> <strong class="hover"></strong></li>
+								<li class="men1"><a  href="#">À propos</a><strong class="hover"></strong></li>
+								<li class="men2"><a  href="boutique.php">Boutique</a> <strong class="hover"></strong></li>
+								<li class="men3"><a  href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>
+								<li class="men4"><a  href="#">Nous Contacter</a> <strong class="hover"></strong></li>
+								<li class="men5"><a  href="#">Votre Panier</a> <strong class="hover"></strong></li>
+							</ul>
+							</ul>
+						</nav>
+						<div class="clear"></div>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</header>
+			<div id="page4" class="content">
+			<div class="container_12">
+				<div class="grid_12">
+					<div class="slogan">
+						<h3>Inscription Réussie</h3>
+							</div>
+							<p align="center">Vous vous êtes bien inscrit, connectez vous <a href="connect\index.html"> ici</a></p>
+				</div>
+				
+			</div>
+		</div> 
 	
-	<section>
-	<p align="center">Vous vous êtes bien inscrit, connectez vous <a href="compte.php"> ici</a></p>
-	</section>
 	
 	
 	
-	
-	
-	
-	
-	
+			<footer>
+			<div class="container_12">
+				<div class="grid_12">
+					<div class="copy">
+						<a href="#"><img src="images/footer_logo.png" alt="" width="60"></a>  &copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="admin/index.php" rel="nofollow">Panel Administrateur</a>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</footer>
 	
 	</body>
 	</html>
