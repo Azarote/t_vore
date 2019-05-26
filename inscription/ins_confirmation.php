31d29e74dea03e61e1654ea0a89f38af7224b9bf
Azarote <Azarotee@gmail.com>
2019-05-26 19:34:05 +0200

    Changement Inscription/Connexion
    -Updata Majeur

--- inscription/ins_confirmation.php
+++ inscription/ins_confirmation.php
@@ -2,12 +2,12 @@
 <head>
 
 	<meta charset="UTF-8">
-		<link rel="icon" href="images/favicon.ico">
-		<link rel="shortcut icon" href="images/favicon.ico" />
-		<link rel="stylesheet" href="css/boutique.css">
-		<link rel="stylesheet" href="css/camera.css">
-		<link rel="stylesheet" href="css/form.css">
-		<link rel="stylesheet" type="text/css" href="css/boutiqueBox.css">
+		<link rel="icon" href="../images/favicon.ico">
+		<link rel="shortcut icon" href="../images/favicon.ico" />
+		<link rel="stylesheet" href="../css/boutique.css">
+		<link rel="stylesheet" href="../css/camera.css">
+		<link rel="stylesheet" href="../css/form.css">
+		<link rel="stylesheet" type="text/css" href="../css/boutiqueBox.css">
 
 	<title>T-VORE Confirmation Inscription</title>
 </head>
@@ -17,14 +17,14 @@
 		<header class="page1">
 			<div class="container_12">
 				<div class="grid_12">
-					<h1><a href="#"><img src="images/logo.png"></a></h1>
+					<h1><a href="#"><img src="../images/logo.png"></a></h1>
 					<div class="menu_block">
 						<nav>
 							<ul class="sf-menu">
-								<li class="current men"><a  href="index.php">Accueil</a> <strong class="hover"></strong></li>
+								<li class="current men"><a  href="../index.php">Accueil</a> <strong class="hover"></strong></li>
 								<li class="men1"><a  href="#">À propos</a><strong class="hover"></strong></li>
-								<li class="men2"><a  href="boutique.php">Boutique</a> <strong class="hover"></strong></li>
-								<li class="men3"><a  href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>
+								<li class="men2"><a  href="../boutique.php">Boutique</a> <strong class="hover"></strong></li>
+								<li class="men3"><a  href="../connect\index.html">Connexion</a> <strong class="hover"></strong></li>
 								<li class="men4"><a  href="#">Nous Contacter</a> <strong class="hover"></strong></li>
 								<li class="men5"><a  href="#">Votre Panier</a> <strong class="hover"></strong></li>
 							</ul>
@@ -42,7 +42,7 @@
 					<div class="slogan">
 						<h3>Inscription Réussie</h3>
 							</div>
-							<p align="center">Vous vous êtes bien inscrit, connectez vous <a href="connect\index.html"> ici</a></p>
+							<p align="center">Vous vous êtes bien inscrit, connectez vous <a href="../connect\index.html"> ici</a></p>
 				</div>
 				
 			</div>
@@ -55,7 +55,7 @@
 			<div class="container_12">
 				<div class="grid_12">
 					<div class="copy">
-						<a href="#"><img src="images/footer_logo.png" alt="" width="60"></a>  &copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="admin/index.php" rel="nofollow">Panel Administrateur</a>
+						<a href="#"><img src="../images/footer_logo.png" alt="" width="60"></a>  &copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="../admin/index.php" rel="nofollow">Panel Administrateur</a>
 					</div>
 				</div>
 				<div class="clear"></div>
