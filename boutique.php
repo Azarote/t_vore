9c70d7c5d434323c7976174c9c4dc4021a018e4b
Azarote <Azarotee@gmail.com>
2019-05-26 21:07:07 +0200

    boutique connexion
    fix

--- boutique.php
+++ boutique.php
@@ -1,29 +1,41 @@
-﻿<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>T-VORE - Boutique</title>
-		<link rel="icon" href="images/favicon.ico">
-		<link rel="shortcut icon" href="images/favicon.ico" />
-		<link rel="stylesheet" href="css/boutique.css">
-		<link rel="stylesheet" href="css/camera.css">
-		<link rel="stylesheet" href="css/form.css">
-		<link rel="stylesheet" type="text/css" href="css/boutiqueBox.css">
-	</head>
+<?php 
+echo '<!DOCTYPE html>';
+echo '<html>';
+	echo '<head>';
+		echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">';
+		echo '<title>T-VORE - Boutique</title>';
+		echo '<link rel="icon" href="images/favicon.ico">';
+		echo '<link rel="shortcut icon" href="images/favicon.ico" />';
+		echo '<link rel="stylesheet" href="css/boutique.css">';
+		echo '<link rel="stylesheet" href="css/camera.css">';
+		echo '<link rel="stylesheet" href="css/form.css">';
+		echo '<link rel="stylesheet" type="text/css" href="css/boutiqueBox.css">';
+	echo '</head>';
 	
-	<body>
+	echo '<body>';
 
-		<header class="page1">
-			<div class="container_12">
-				<div class="grid_12">
-					<h1><a href="#"><img src="images/logo.png"></a></h1>
-					<div class="menu_block">
-						<nav>
-							<ul class="sf-menu">
-								<li class="current men"><a  href="index.php">Accueil</a> <strong class="hover"></strong></li>
-								<li class="men1"><a  href="index.php #page3">À propos</a><strong class="hover"></strong></li>
-								<li class="men2"><a  href="boutique.php">Boutique</a> <strong class="hover"></strong></li>
-								<li class="men3"><a  href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>
+		echo '<header class="page1">';
+			echo '<div class="container_12">';
+				echo '<div class="grid_12">';
+					echo '<h1><a href="#"><img src="images/logo.png"></a></h1>';
+					echo '<div class="menu_block">';
+						echo '<nav>';
+							echo '<ul class="sf-menu">';
+								echo '<li class="current men"><a  href="index.php">Accueil</a> <strong class="hover"></strong></li>';
+								echo '<li class="men1"><a  href="index.php #page3">À propos</a><strong class="hover"></strong></li>';
+								echo '<li class="men2"><a  href="boutique.php">Boutique</a> <strong class="hover"></strong></li>';
+								if (empty($_SESSION['admin'])) {
+									echo '<li class="men3"><a  href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>';
+									
+								
+									}
+
+									else {
+								echo '<li class="men3"><a href="deconnexion.php">Déconnexion</a> <strong class="hover"></strong></li>';
+								
+								 
+								}
+								?>
 								<li class="men4"><a  href="index.php #page4">Nous Contacter</a> <strong class="hover"></strong></li>
 								<li class="men5"><a  href="panier.php">Votre Panier</a> <strong class="hover"></strong></li>
 							</ul>
