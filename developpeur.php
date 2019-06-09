73bdd8ad0d4059724404df18cdf20c9d506d1491
TheSmallJudi <49020754+TheSmallJudi@users.noreply.github.com>
2019-06-09 22:25:18 +0200

    opti code2
    •Ajout de commentaires
    •indentation

--- developpeur.php
+++ developpeur.php
@@ -1,4 +1,4 @@
-﻿<?php  
+﻿<?php
 session_start();
 echo '<!DOCTYPE html>';
 echo '<html>';
@@ -12,7 +12,7 @@ echo '<html>';
 		echo '<link rel="stylesheet" href="css/form.css">';
 		echo '<link rel="stylesheet" href="css/developpeur.css">';
 	echo '</head>';
-	
+
 	echo '<body>';
 
 		echo '<header class="page1">';
@@ -22,24 +22,18 @@ echo '<html>';
 					echo '<div class="menu_block">';
 						echo '<nav>';
 							echo '<ul class="sf-menu">';
-								echo '<li class="current men"><a  href="index.php">Accueil</a> <strong class="hover"></strong></li>';
-								echo '<li class="men1"><a  href="index.php #page3">À propos</a><strong class="hover"></strong></li>';
-								echo '<li class="men2"><a  href="boutique.php">Boutique</a> <strong class="hover"></strong></li>';
+								echo '<li class="current men"><a	href="index.php">Accueil</a> <strong class="hover"></strong></li>';
+								echo '<li class="men1"><a	href="index.php #page3">À propos</a><strong class="hover"></strong></li>';
+								echo '<li class="men2"><a	href="boutique.php">Boutique</a> <strong class="hover"></strong></li>';
 								if (empty($_SESSION['admin'])) {
-									echo '<li class="men3"><a  href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>';
-									
-								
-									}
-
-									else {
-								echo '<li class="men3"><a href="deconnexion.php">Déconnexion</a> <strong class="hover"></strong></li>';
-								
-								 
+									echo '<li class="men3"><a	href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>';
+								}
+								else {
+									echo '<li class="men3"><a href="deconnexion.php">Déconnexion</a> <strong class="hover"></strong></li>';
 								}
 								?>
-								<li class="men4"><a  href="index.php #page4">Nous Contacter</a> <strong class="hover"></strong></li>
-								<li class="men5"><a  href="panier.php">Votre Panier</a> <strong class="hover"></strong></li>
-							</ul>
+								<li class="men4"><a	href="index.php #page4">Nous Contacter</a> <strong class="hover"></strong></li>
+								<li class="men5"><a	href="panier.php">Votre Panier</a> <strong class="hover"></strong></li>
 							</ul>
 						</nav>
 						<div class="clear"></div>
@@ -53,16 +47,17 @@ echo '<html>';
 	if (isset($_GET['ref']))
 	{
 	$numRef=$_GET['ref'];
-	
+
 	$db=mysql_connect("localhost","root","admin") or die("erreur de connection".mysql_error());		/*connection au serveur MySQL*/
 	mysql_select_db("TVORE",$db) or die("Erreur de connection à la base T-VORE");		/*ouverture de la base TVORE*/
+	// Tout ce qui concerne la référence $numRef
 	$resultat = mysql_query("SELECT r.numTshirt, r.numCouleur, r.stock, t.nomtshirt, t.prix, c.libelleCouleur, ta.libelleTaille
-		                     FROM reftshirt AS r
-		                     INNER JOIN tshirt AS t ON t.numTshirt = r.numTshirt
-		                     INNER JOIN couleur AS c ON c.numCouleur = r.numCouleur
-		                     INNER JOIN taille AS ta ON ta.numTaille = r.numTaille
-		                     WHERE r.numRef=$numRef") or die("erreur requete");
-	while ($ligne=mysql_fetch_assoc($resultat)) 
+							 FROM reftshirt AS r
+							 INNER JOIN tshirt AS t ON t.numTshirt = r.numTshirt
+							 INNER JOIN couleur AS c ON c.numCouleur = r.numCouleur
+							 INNER JOIN taille AS ta ON ta.numTaille = r.numTaille
+							 WHERE r.numRef=$numRef") or die("erreur requete");
+	while ($ligne=mysql_fetch_assoc($resultat))
 	{
 		$numTshirt=$ligne["numTshirt"];
 		$numCouleur=$ligne["numCouleur"];
@@ -75,75 +70,73 @@ echo '<html>';
 
 
 	echo "<div id='page2' class='content'>
-		  <div class='container_12'>
+			<div class='container_12'>
 				<div class='grid_12'>
 						<div class='slogan'>
 							<h3>$nomtshirt $libelleCouleur $libelleTaille</h3>
 							<h3>Quantité en stock $stock</h3>
 						</div>
 					</div>
-			
+
 			<div class='photoDiv'>";
 
 	$resultat = mysql_query("SELECT cheminImage FROM photo WHERE numTshirt=$numTshirt and numCouleur=$numCouleur") or die("erreur requete");
-	while ($ligne=mysql_fetch_assoc($resultat)) 
+	while ($ligne=mysql_fetch_assoc($resultat))
 	{
 		$cheminImage=$ligne["cheminImage"];
-		echo "<div class='photo1'>	
+		echo "<div class='photo1'>
 				<img src='photos/$cheminImage'>
-  			  </div>";
-  	}
-  	echo "</div>	
+					</div>";
+		}
+		echo "</div>
 
 				<h2>$prix €</h2>";
 
 	// On recherche les autres références
 	$requete = "SELECT r.numRef, r.stock, c.libelleCouleur, ta.libelleTaille
-                FROM reftshirt AS r
-                INNER JOIN couleur AS c ON c.numCouleur = r.numCouleur
-                INNER JOIN taille AS ta ON ta.numTaille = r.numTaille
-                WHERE r.numTshirt=$numTshirt AND r.numRef != $numRef
-                ORDER BY c.libelleCouleur, ta.numTaille";
+				FROM reftshirt AS r
+				INNER JOIN couleur AS c ON c.numCouleur = r.numCouleur
+				INNER JOIN taille AS ta ON ta.numTaille = r.numTaille
+				WHERE r.numTshirt=$numTshirt AND r.numRef != $numRef
+				ORDER BY c.libelleCouleur, ta.numTaille";
 	$resultat = mysql_query($requete) or die("erreur requete");
-
-    // Tester si il y a un résultat
 
 	echo "<div>D'autres références existent pour cet article :</div>
 
-	    <table>
-	        <tr class='refCol'><th> Référence </th> <th> Couleur </th> <th> Taille </th> <th> Quantité </th></tr>
-	     ";
-	while ($ligne=mysql_fetch_assoc($resultat)) 
+			<table>
+					<tr class='refCol'><th> Référence </th> <th> Couleur </th> <th> Taille </th> <th> Quantité </th></tr>
+			 ";
+	while ($ligne=mysql_fetch_assoc($resultat))
 	{
 		$numRef2=$ligne["numRef"];
 		$stock=$ligne["stock"];
 		$libelleCouleur=$ligne["libelleCouleur"];
 		$libelleTaille=$ligne["libelleTaille"];
-	    echo '<tr><td class="refCol2"><a href="?ref='.$numRef2.'">'.$numRef2.'</a></td><td>'.$libelleCouleur.'</td><td>'.$libelleTaille.'</td><td>'.$stock.'</td></tr>
-	         ';
-  	}
-  	echo "</table>";
+			echo '<tr><td class="refCol2"><a href="?ref='.$numRef2.'">'.$numRef2.'</a></td><td>'.$libelleCouleur.'</td><td>'.$libelleTaille.'</td><td>'.$stock.'</td></tr>
+					 ';
+		}
+		echo "</table>";
 
 	echo '<div>
-	        <form method="post" action="panier.php">
-	          <input type="hidden" value="'.$numRef.'" name="ref">
-	          <input type="submit" value="Ajouter au panier" name="ajoutPanier" class="validDev">
-	        </form>
-	      </div>
+					<form method="post" action="panier.php">
+						<input type="hidden" value="'.$numRef.'" name="ref">
+						<input type="submit" value="Ajouter au panier" name="ajoutPanier" class="validDev">
+					</form>
+				</div>
 	</div>
 </div>';
 
 		mysql_close($db); /*fermeture de MySQL*/
-    }
+		}
 ?>
 
-		
+
 
 		<footer>
 			<div class="container_12">
 				<div class="grid_12">
 					<div class="copy">
-						<a href="#"><img src="images/footer_logo.png" alt="" width="60"></a>  &copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="admin/index.php" rel="nofollow">Panel Administrateur</a>
+						<a href="#"><img src="images/footer_logo.png" alt="" width="60"></a>	&copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="admin/index.php" rel="nofollow">Panel Administrateur</a>
 					</div>
 				</div>
 				<div class="clear"></div>
--- panier.php
+++ panier.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 session_start();
 echo '<!DOCTYPE html>';
 echo '<html>';
@@ -12,7 +12,7 @@ echo '<html>';
 		echo '<link rel="stylesheet" href="css/form.css">';
 		echo '<link rel="stylesheet" href="css/developpeur.css">';
 	echo '</head>';
-	
+
 	echo '<body>';
 
 		echo '<header class="page1">';
@@ -22,24 +22,18 @@ echo '<html>';
 					echo '<div class="menu_block">';
 						echo '<nav>';
 							echo '<ul class="sf-menu">';
-								echo '<li class="current men"><a  href="index.php">Accueil</a> <strong class="hover"></strong></li>';
-								echo '<li class="men1"><a  href="index.php #page3">À propos</a><strong class="hover"></strong></li>';
-								echo '<li class="men2"><a  href="boutique.php">Boutique</a> <strong class="hover"></strong></li>';
+								echo '<li class="current men"><a	href="index.php">Accueil</a> <strong class="hover"></strong></li>';
+								echo '<li class="men1"><a	href="index.php #page3">À propos</a><strong class="hover"></strong></li>';
+								echo '<li class="men2"><a	href="boutique.php">Boutique</a> <strong class="hover"></strong></li>';
 								if (empty($_SESSION['admin'])) {
-									echo '<li class="men3"><a  href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>';
-									
-								
-									}
-
-									else {
-								echo '<li class="men3"><a href="deconnexion.php">Déconnexion</a> <strong class="hover"></strong></li>';
-								
-								 
+									echo '<li class="men3"><a	href="connect\index.html">Connexion</a> <strong class="hover"></strong></li>';
+								}
+								else {
+									echo '<li class="men3"><a href="deconnexion.php">Déconnexion</a> <strong class="hover"></strong></li>';
 								}
 								?>
-								<li class="men4"><a  href="index.php #page4">Nous Contacter</a> <strong class="hover"></strong></li>
-								<li class="men5"><a  href="panier.php">Votre Panier</a> <strong class="hover"></strong></li>
-							</ul>
+								<li class="men4"><a	href="index.php #page4">Nous Contacter</a> <strong class="hover"></strong></li>
+								<li class="men5"><a	href="panier.php">Votre Panier</a> <strong class="hover"></strong></li>
 							</ul>
 						</nav>
 						<div class="clear"></div>
@@ -49,11 +43,11 @@ echo '<html>';
 			</div>
 		</header>
 
-	<?php
-	  $IdClient = $_SESSION["client"];
-      $db=mysql_connect("localhost","root","admin") or die("erreur de connection".mysql_error());		/*connection au serveur MySQL*/
-   	  mysql_select_db("TVORE",$db) or die("Erreur de connection à la base T-VORE");		/*ouverture de la base TVORE*/
-   	?>
+		<?php
+			$IdClient = $_SESSION["client"];
+			$db=mysql_connect("localhost","root","admin") or die("erreur de connection".mysql_error());		/*connection au serveur MySQL*/
+			mysql_select_db("TVORE",$db) or die("Erreur de connection à la base T-VORE");		/*ouverture de la base TVORE*/
+		 ?>
 		<div id="page2" class="content" id="personnel">
 			<div class="container_12">
 				<div class="grid_12">
@@ -64,63 +58,65 @@ echo '<html>';
 							echo "Vous n'êtes pas connecté à votre compte. <a href='connect/index.html'>Connectez vous ici</a>";
 						}
 						else {
-						  $IdClient = $_SESSION["client"];
-						  if (isset($_POST['ajoutPanier']))
-						  {
-						  	$numRef = $_POST['ref'];
-						    mysql_query("INSERT INTO panier (IdClient, numRef) VALUES ($IdClient, $numRef)") or die("erreur requete");
-						  }
-						  $requete = "SELECT p.numRef, r.stock, t.nomTshirt, t.prix, c.libelleCouleur, ta.libelleTaille
-						              FROM panier AS p
-						              INNER JOIN reftshirt AS r ON r.numRef = p.numRef
-						              INNER JOIN tshirt AS t ON t.numTshirt = r.numTshirt
-						              INNER JOIN couleur AS c ON c.numCouleur = r.numCouleur
-						              INNER JOIN taille AS ta ON ta.numTaille = r.numTaille
-						              WHERE p.IdClient = $IdClient
-						              ORDER BY t.nomTshirt, c.libelleCouleur, ta.numTaille";
-						  $resultat = mysql_query($requete) or die("erreur requete");
-						  echo "<table>
-						          <tr class='refCol'><th>Référence</th><th>Nom</th><th>Couleur</th><th>Taille</th><th>Prix</th></tr>
-						       ";
-							while ($ligne=mysql_fetch_assoc($resultat)) 
+							$IdClient = $_SESSION["client"];
+							if (isset($_POST['ajoutPanier']))
+							{
+								$numRef = $_POST['ref'];
+								mysql_query("INSERT INTO panier (IdClient, numRef) VALUES ($IdClient, $numRef)") or die("erreur requete");
+							}
+							// Tout ce qui est dans le panier du client $IdClient
+							$requete = "SELECT p.numRef, r.stock, t.nomTshirt, t.prix, c.libelleCouleur, ta.libelleTaille
+										FROM panier AS p
+										INNER JOIN reftshirt AS r ON r.numRef = p.numRef
+										INNER JOIN tshirt AS t ON t.numTshirt = r.numTshirt
+										INNER JOIN couleur AS c ON c.numCouleur = r.numCouleur
+										INNER JOIN taille AS ta ON ta.numTaille = r.numTaille
+										WHERE p.IdClient = $IdClient
+										ORDER BY t.nomTshirt, c.libelleCouleur, ta.numTaille";
+							$resultat = mysql_query($requete) or die("erreur requete");
+							echo "<table>
+											<tr class='refCol'><th>Référence</th><th>Nom</th><th>Couleur</th><th>Taille</th><th>Prix</th></tr>
+									 ";
+							while ($ligne=mysql_fetch_assoc($resultat))
 							{
 								$numRef=$ligne["numRef"];
 								$nomTshirt=utf8_encode($ligne["nomTshirt"]);
 								$libelleCouleur=$ligne["libelleCouleur"];
 								$libelleTaille=$ligne["libelleTaille"];
 								$prix=$ligne["prix"];
-							    echo '<tr><td class="refCol2"><a href="?ref='.$numRef.'">'.$numRef.'</a></td><td>'.$nomTshirt.'</td><td>'.$libelleCouleur.'</td><td>'.$libelleTaille.'</td><td>'.$prix.'</td></tr>
-							         ';
-						  	}
-						  	echo "</table>
-						  	     ";
+									echo '<tr><td class="refCol2"><a href="?ref='.$numRef.'">'.$numRef.'</a></td><td>'.$nomTshirt.'</td><td>'.$libelleCouleur.'</td><td>'.$libelleTaille.'</td><td>'.$prix.'</td></tr>
+											 ';
+								}
+								echo "</table>
+										 ";
 
-						  $requete = "SELECT SUM(t.prix) AS total
-						              FROM panier AS p
-						              INNER JOIN reftshirt AS r ON r.numRef = p.numRef
-						              INNER JOIN tshirt AS t ON t.numTshirt = r.numTshirt
-						              WHERE p.IdClient = $IdClient";
-						  $resultat = mysql_query($requete) or die("erreur requete");
-							while ($ligne=mysql_fetch_assoc($resultat)) 
+							// Une seconde requête pour avoir le prix total
+							$requete = "SELECT SUM(t.prix) AS total
+										FROM panier AS p
+										INNER JOIN reftshirt AS r ON r.numRef = p.numRef
+										INNER JOIN tshirt AS t ON t.numTshirt = r.numTshirt
+										WHERE p.IdClient = $IdClient";
+							$resultat = mysql_query($requete) or die("erreur requete");
+							while ($ligne=mysql_fetch_assoc($resultat))
 							{
 								$total=$ligne["total"];
-							    echo "<h2>Prix total : $total</h2>";
-						  	}
+								echo "<h2>Prix total : $total</h2>";
+							}
 
 							echo '<div>
-							        <form method="post" action="finCommande.php">
-							          <input type="submit" value="Valider la commande" name="valider" class="validDev">
-							        </form>
-							      </div>';
+									<form method="post" action="finCommande.php">
+										<input type="submit" value="Valider la commande" name="valider" class="validDev">
+									</form>
+								</div>';
 
-                     	  mysql_close($db); /*fermeture de MySQL*/
-                     	  }
-						?>
-						
+							mysql_close($db); /*fermeture de MySQL*/
+						}
+					?>
+
 					</div>
 				</div>
-				
-				
+
+
 			</div>
 		</div>
 
@@ -128,7 +124,7 @@ echo '<html>';
 			<div class="container_12">
 				<div class="grid_12">
 					<div class="copy">
-						<a href="#"><img src="images/footer_logo.png" alt="" width="60"></a>  &copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="admin/index.php" rel="nofollow">Panel Administrateur</a>
+						<a href="#"><img src="images/footer_logo.png" alt="" width="60"></a>	&copy; 2019 | <a href="#">Privacy Policy</a> <br> Website designed by Judicael/Matteo <a href="admin/index.php" rel="nofollow">Panel Administrateur</a>
 					</div>
 				</div>
 				<div class="clear"></div>
